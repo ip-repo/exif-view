b46cac72c9ae640e32f101bab29b34515e7e8175
ip-repo <123945379+ip-repo@users.noreply.github.com>
2024-03-06 23:20:13 +0200

    Create html_parts.py

---
+++ components/html_parts.py
@@ -0,0 +1,42 @@
+html_part_one = [
+			"<!DOCTYPE html>",
+			"<html>",
+			"<head>",
+			"<style>",
+			"body {background-color:white;}",
+			"h2 {color:black;}",
+			"  .key-value-container {",
+			"	 table-layout:fixed;"
+			"    font-family: 'Arial', sans-serif;",
+			"    border: 1px solid #ddd;",
+			"    border-collapse: collapse;",
+			"    margin-bottom: 20px;",
+			"	 margin: auto;", 
+    		
+			"  }",
+			"  .key-value-container th, .key-value-container td {",
+			"    border: 1px solid lightgray;",
+			"    padding: 8-px;",
+			"    text-align: left;",
+			"    background-color: lightgreen;",
+			"    color:black",
+			"  }",
+			"  .key-value-container th {",
+			"    background-color:aquamarine;",
+			"    color: black",
+			
+			"  }",
+			
+			"</style>",
+			"</head>",
+			"<body>",
+			"<div style='text-align: center;'>"]
+
+html_part_two = [ "<table class='key-value-container'>",
+			"  <tr>",
+			"    <th>Key</th>",
+			"    <th>Value</th>",
+			"  </tr>",
+			]
+
+			
